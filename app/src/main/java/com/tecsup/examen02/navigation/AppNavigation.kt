1bb56368ee5da8b7538889b17f081066fdbbd004
b127 <xiomara_mercedes_123@hotmail.com>
2026-04-29 18:42:04 -0500

    Conecto las 5 pantallas con NavHost completo

--- app/src/main/java/com/tecsup/examen02/navigation/AppNavigation.kt
+++ app/src/main/java/com/tecsup/examen02/navigation/AppNavigation.kt
@@ -6,11 +6,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.tecsup.examen02.screens.LoginScreen
 import com.tecsup.examen02.screens.HomeScreen
+import com.tecsup.examen02.screens.MenuScreen
+import com.tecsup.examen02.screens.DetalleScreen
+import com.tecsup.examen02.screens.PerfilScreen
 
 @Composable
 fun AppNavigation() {
 
     val navController = rememberNavController()
+    var pedido by remember { mutableStateOf(mapOf<Int, Int>()) }
 
     NavHost(navController = navController, startDestination = "login") {
 
@@ -22,9 +26,37 @@ fun AppNavigation() {
 
         composable("home") {
             HomeScreen(
-                onVerMenu = { },
-                onVerPedido = { },
-                onVerPerfil = { }
+                onVerMenu = { navController.navigate("menu") },
+                onVerPedido = { navController.navigate("perfil") },
+                onVerPerfil = { navController.navigate("perfil") }
+            )
+        }
+
+        composable("menu") {
+            MenuScreen(
+                onPlatoClick = { id -> navController.navigate("detalle/$id") },
+                onBack = { navController.popBackStack() }
+            )
+        }
+
+        composable("detalle/{platoId}") { backStackEntry ->
+            val platoId = backStackEntry.arguments?.getString("platoId")?.toInt() ?: 1
+            DetalleScreen(
+                platoId = platoId,
+                onAgregar = { id, cantidad ->
+                    val nuevoPedido = pedido.toMutableMap()
+                    nuevoPedido[id] = (nuevoPedido[id] ?: 0) + cantidad
+                    pedido = nuevoPedido
+                    navController.popBackStack()
+                },
+                onBack = { navController.popBackStack() }
+            )
+        }
+
+        composable("perfil") {
+            PerfilScreen(
+                pedido = pedido,
+                onBack = { navController.popBackStack() }
             )
         }
     }
